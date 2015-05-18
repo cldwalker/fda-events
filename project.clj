1ac528b8824d72459dfe31f24cb0dfdd553a3131
Gabriel Horner <gabriel.horner@gmail.com>
2015-05-18 12:06:58 -0400

    Wire up rum with basic input, app-state and message div

--- project.clj
+++ project.clj
@@ -3,11 +3,12 @@
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2755"]]
+                  [org.clojure/clojurescript "0.0-3149" :scope "provided"]
+                  [rum "0.2.6"]]
 
   :node-dependencies [[source-map-support "0.2.8"]]
 
-  :plugins [[lein-cljsbuild "1.0.4"]
+  :plugins [[lein-cljsbuild "1.0.5"]
             [lein-npm "0.4.0"]]
 
   :source-paths ["src" "target/classes"]
--- src/fda_events/core.cljs
+++ src/fda_events/core.cljs
@@ -1,8 +1,35 @@
 (ns fda-events.core
-  (:require [clojure.browser.repl :as repl]))
+  (:require [clojure.browser.repl :as repl]
+            [rum]))
 
 ;; (repl/connect "http://localhost:9000/repl")
 
 (enable-console-print!)
 
-(println "Hello world!")
+(def app-state (atom {}))
+
+(defn submit-query [app event]
+  (swap! app assoc :message "Submitted!")
+  (.preventDefault event))
+
+(rum/defc query-form [app]
+  [:form
+   [:.row
+    [:.small-8
+     [:.row
+      [:.small-9.columns
+       [:input.radius {:type "text" :id "query" :placeholder "Query"}]]
+      [:.small-3.columns
+       [:a.button.small.radius
+        {:href "#" :onClick #(submit-query app %)}
+        "Search"]]]]]])
+
+(rum/defc fda-event-app < rum/reactive [app]
+  (let [{:keys [message]} (rum/react app)]
+    [:div
+     [:h2 "Search FDA Events!"]
+     (when message
+       [:div.alert-box.radius.small-6.columns {:data-alert true} message])
+     (query-form app)]))
+
+(rum/mount (fda-event-app app-state) (.getElementById js/document "app"))
